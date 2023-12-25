7f99126503e5bfc06074cc29b081902945e01d86
onfarmview <thaitranphd@gmail.com>
2023-12-25 13:24:17 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,52 @@
+# import streamlit as st
+# from streamlit_folium import folium_static
+# import folium
+
+# "# streamlit-folium"
+
+# # with st.echo():
+# import streamlit as st
+# from streamlit_folium import folium_static
+# import folium
+
+# # center on Liberty Bell
+# m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+
+# # add marker for Liberty Bell
+# tooltip = "Liberty Bell"
+# folium.Marker(
+#     [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+# ).add_to(m)
+
+# # call to render Folium map in Streamlit
+# folium_static(m)
+
 import streamlit as st
 from streamlit_folium import folium_static
-import folium
+import geemap.eefolium as geemap
+import ee
 
-"# streamlit-folium"
+# os.environ["EARTHENGINE_TOKEN"] == st.secrets["EARTHENGINE_TOKEN"]
 
-# with st.echo():
-import streamlit as st
-from streamlit_folium import folium_static
-import folium
+"# streamlit geemap demo"
+st.markdown('Source code: <https://github.com/giswqs/geemap-streamlit/blob/main/geemap_app.py>')
 
-# center on Liberty Bell
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+with st.echo():
+    import streamlit as st
+    from streamlit_folium import folium_static
+    import geemap.eefolium as geemap
+    import ee
 
-# add marker for Liberty Bell
-tooltip = "Liberty Bell"
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-).add_to(m)
+    m = geemap.Map()
+    dem = ee.Image('USGS/SRTMGL1_003')
 
-# call to render Folium map in Streamlit
-folium_static(m)
+    vis_params = {
+    'min': 0,
+    'max': 4000,
+    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
+
+    m.addLayer(dem, vis_params, 'SRTM DEM', True, 1)
+    m.addLayerControl()
+
+    # call to render Folium map in Streamlit
+    folium_static(m)
